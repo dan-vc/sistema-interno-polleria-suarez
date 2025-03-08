7fcf19dd46597205a5dc7ec0eb771f49f228c052
Daniel Villafranqui <danedo20472@gmail.com>
2025-03-08 15:17:31 -0500

    Sidebar Loader w/ JS added

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,14 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.sidebar').classList.add('open');
 
         const backdrop = document.createElement('div');
-        backdrop.style.width = '100%';
-        backdrop.style.height = '100%';
-        backdrop.style.position = 'fixed';
-        backdrop.style.top = '0';
-        backdrop.style.left = '0';
-        backdrop.style.zIndex = '998';
-        backdrop.style.background = 'rgb(0, 0, 0, 0.5)';
-
+        backdrop.id = "backdrop";
         document.body.appendChild(backdrop);
 
 
